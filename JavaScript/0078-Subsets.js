4397bbc381b9f69afa94ffa7c85327ec860e109b
izferriss <ferrissi@gmail.com>
2023-03-12 13:03:17 -0400

    Add files via Visual Studio Code

---
+++ JavaScript/0078-Subsets.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/subsets/
+
+// Given an integer array nums of unique elements, return all possible 
+// subsets
+//  (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [[],[0]]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+// All the numbers of nums are unique.
+
+let subsets = function(nums)
+{
+    let result = [];
+    backtrack(result, [], nums, 0);
+    return result;
+}
+
+function backtrack(arr, tmpArr, nums, start)
+{
+    arr.push([...tmpArr]);
+    for(let i = start; i < nums.length; i++)
+    {
+        tmpArr.push(nums[i]);
+        backtrack(arr, tmpArr, nums, i + 1);
+        tmpArr.pop();
+    }
+}
